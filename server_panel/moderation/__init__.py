940f2ff7be2dce40c74614248533ccd124440ad7
T.J. Tarazevits <tarazevits@gmail.com>
2026-08-03 14:21:24 -0500

    Preserve typed Workshop service compatibility

--- server_panel/moderation/__init__.py
+++ server_panel/moderation/__init__.py
@@ -3,7 +3,7 @@
 from .background import ModerationPoller
 from .blueprint import create_routes
 from .installer import ModerationInstaller
-from .models import InstallRequest, ServiceResult, TicketAction, sanitize_dll_url
+from .models import InstallRequest, ModerationSettingsUpdate, ServiceResult, TicketAction, sanitize_dll_url
 from .repository import ModerationRepository
 from .service import ModerationService
 
@@ -13,6 +13,7 @@ __all__ = [
     "ModerationPoller",
     "ModerationRepository",
     "ModerationService",
+    "ModerationSettingsUpdate",
     "ServiceResult",
     "TicketAction",
     "create_routes",
