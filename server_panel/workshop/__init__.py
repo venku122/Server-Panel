d24cda12ada21384ecb85b7f95da060692a4906b
T.J. Tarazevits <tarazevits@gmail.com>
2026-08-05 00:18:08 -0500

    Make Workshop rotation mutations transactional

--- server_panel/workshop/__init__.py
+++ server_panel/workshop/__init__.py
@@ -1,15 +1,21 @@
 """Local Steam Workshop library services."""
 
 from .library import (
+    WorkshopCandidate,
     WorkshopItem,
     WorkshopLibrary,
+    WorkshopMutationPlan,
     WorkshopReference,
+    invalidate_workshop_cache,
     parse_workshop_reference,
 )
 
 __all__ = [
+    "WorkshopCandidate",
     "WorkshopItem",
     "WorkshopLibrary",
+    "WorkshopMutationPlan",
     "WorkshopReference",
+    "invalidate_workshop_cache",
     "parse_workshop_reference",
 ]
